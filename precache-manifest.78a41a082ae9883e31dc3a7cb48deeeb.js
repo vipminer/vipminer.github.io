054162f23def378a71039917c70bd4d2552e12ea
vipminer <vipminer@gmail.com>
2021-10-02 08:24:27 -0700

    update

---
+++ precache-manifest.78a41a082ae9883e31dc3a7cb48deeeb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c099c5f1b2b4e4fdfa3",
+    "url": "/static/css/main.86464017.chunk.css"
+  },
+  {
+    "revision": "2c099c5f1b2b4e4fdfa3",
+    "url": "/static/js/main.2c099c5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "ca9f50ec9fe380dedcf0",
+    "url": "/static/css/2.c0aa52ac.chunk.css"
+  },
+  {
+    "revision": "ca9f50ec9fe380dedcf0",
+    "url": "/static/js/2.ca9f50ec.chunk.js"
+  },
+  {
+    "revision": "9b9000a40d491a701424e94826c7ffe9",
+    "url": "/static/media/minerLogo.9b9000a4.png"
+  },
+  {
+    "revision": "c70f9c7b2f5e033c59210103237f6d2b",
+    "url": "/static/media/logo.c70f9c7b.png"
+  },
+  {
+    "revision": "ed2d82776931fc43dbbbf5b9a514cfcf",
+    "url": "/static/media/hire.ed2d8277.png"
+  },
+  {
+    "revision": "7998605189750745871dff452c26df69",
+    "url": "/static/media/sell.79986051.png"
+  },
+  {
+    "revision": "9b9000a40d491a701424e94826c7ffe9",
+    "url": "/static/media/banner.9b9000a4.png"
+  },
+  {
+    "revision": "bf94dd3b67d8816105e232e9c8f9459b",
+    "url": "/index.html"
+  }
+];
